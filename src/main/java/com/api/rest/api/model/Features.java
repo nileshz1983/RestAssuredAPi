25cf120d6c5a0b62b35ffca3e4c6ef117f3780c9
snehali <snehalikhakse88@gmail.com>
2018-11-04 18:00:26 +0530

    added project

---
+++ src/main/java/com/api/rest/api/model/Features.java
@@ -0,0 +1,21 @@
+package com.api.rest.api.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Features {
+	
+	private List<String> Feature = new ArrayList<String>();
+	
+	@JsonProperty("Feature")
+	public List<String> getFeature() {
+		return Feature;
+	}
+
+	public void setFeature(List<String> feature) {
+		Feature = feature;
+	}
+
+}
